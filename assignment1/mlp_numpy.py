fixed numerical error! one-hot encode the labels

assignment1/mlp_numpy.py
@@ -34,18 +34,25 @@ class MLP(object):
     Once initialized an MLP object can perform forward and backward.
     """
 
-    input_layer: m.LinearModule
-    hidden_layers: List[Tuple[m.ELUModule, m.LinearModule]]
+    layers: List[Tuple[m.ELUModule, m.LinearModule]]
     softmax_module: m.SoftMaxModule
 
     def __init__(self, n_inputs: int, n_hidden: List[int], n_classes: int):
         n_hidden.append(n_classes)
-        self.input_layer = m.LinearModule(n_inputs, n_hidden[0], input_layer=True)
-        self.hidden_layers = [
+        self.layers = [(None, m.LinearModule(n_inputs, n_hidden[0], input_layer=True))]
+        self.layers += [
             (m.ELUModule(), m.LinearModule(*dims))
             for dims in zip(n_hidden, n_hidden[1:])
         ]
         self.softmax_module = m.SoftMaxModule()
+
+    @property
+    def input_layer(self) -> m.LinearModule:
+        return self.layers[0][1]
+
+    @property
+    def hidden_layers(self) -> List[Tuple[m.CrossEntropyModule, m.LinearModule]]:
+        return self.layers[1:]
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         """
@@ -86,17 +93,14 @@ class MLP(object):
             linear.clear_cache()
 
     def state_dict(self) -> List[Tuple[np.ndarray, np.ndarray]]:
-        weights, biases = [], []
-        for param, store in zip(["weight", "bias"], [weights, biases]):
-            store.append(self.input_layer.params[param])
-            for _, linear in self.hidden_layers:
-                store.append(linear.params[param])
-        return deepcopy(zip(weights, biases))
+        return deepcopy(
+            [
+                (linear.params["weight"], linear.params["bias"])
+                for _, linear in self.layers
+            ]
+        )
 
     def load_state_dict(self, state_dict: List[Tuple[np.ndarray, np.ndarray]]):
-        (in_weight, in_bias), *rest_params = state_dict
-        self.input_layer.params["weight"] = in_weight
-        self.input_layer.params["bias"] = in_bias
-        for (_, linear), (weight, bias) in zip(self.hidden_layers, rest_params):
+        for (_, linear), (weight, bias) in zip(self.layers, state_dict):
             linear.params["weight"] = weight
             linear.params["bias"] = bias

assignment1/modules.py
@@ -21,6 +21,8 @@ from typing import Dict
 
 import numpy as np
 
+import utils as u
+
 
 class LinearModule(object):
     """
@@ -195,7 +197,7 @@ class CrossEntropyModule(object):
         Returns:
           out: cross entropy loss
         """
-        return -np.sum(np.log(x).T * y) / x.shape[0]
+        return -np.sum(u.one_hot(x.shape[1], y) * np.log(x)) / x.shape[0]
 
     def backward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
         """
@@ -206,4 +208,4 @@ class CrossEntropyModule(object):
         Returns:
           dx: gradient of the loss with the respect to the input x.
         """
-        return y[:, None] / (x * -x.shape[0])
+        return -u.one_hot(x.shape[1], y) / (x * x.shape[0])

assignment1/train_mlp_numpy.py
@@ -21,7 +21,6 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 import os
-from collections import OrderedDict
 from typing import List, Tuple
 
 import numpy as np
@@ -64,9 +63,8 @@ def evaluate_model(model: mlp.MLP, data_loader: data.DataLoader, num_classes=10)
 
 def optimize(model: mlp.MLP, lr: float = 0.1, **_):
     for param in ["weight", "bias"]:
-        model.input_layer.params[param] += lr * model.input_layer.grads[param]
-        for _, linear in model.hidden_layers:
-            linear.params[param] += lr * linear.grads[param]
+        for _, linear in model.layers:
+            linear.params[param] -= lr * linear.grads[param]
 
 
 def train_one_epoch(
@@ -137,7 +135,7 @@ def train(
         **kwargs,
     }
 
-    best_params: OrderedDict = None
+    best_params = None
     train_losses, val_losses, val_accuracies = [], [], []
     for epoch in range(epochs):
         logger.debug("Epoch: %s", epoch)
@@ -213,17 +211,26 @@ model, val_accuracies, test_accuracy, info = train(
 )
 
 # model = mlp.MLP(32 * 32 * 3, [4], 10)
+# # model = mlp.MLP(10, [], 10)
 # loss_module = m.CrossEntropyModule()
+# # xs, labs = np.random.randint(0, 10, size=(3, 10)), np.random.randint(0, 10, size=(3,))
 # data_dir = "data"
-# batch_size = 4
+# batch_size = 128
 # cifar10 = cifar10_utils.get_cifar10(data_dir)
 # cifar10_loader = cifar10_utils.get_dataloader(
 #     cifar10, batch_size=batch_size, return_numpy=True
 # )
 # xs, labs = next(iter(cifar10_loader["train"]))
 # preds = model.forward(xs)
-# preds
-# loss = loss_module.forward(preds, labs)
-# loss
-# loss_grad = loss_module.backward(preds, labs)
-# in_grad = model.backward(loss_grad)
+# # preds
+# # model.input_layer.grads["weight"]
+# # loss = loss_module.forward(preds, labs)
+# # loss
+# # loss_grad = loss_module.backward(preds, labs)
+# # in_grad = model.backward(loss_grad)
+# # model.input_layer.params["weight"]
+# # model.input_layer.grads["weight"]
+
+
+# optimize(model, lr=0.1)
+# model.input_layer.params["weight"]