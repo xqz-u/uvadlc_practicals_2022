8d367849b9ec2b103e7bae67d1ce6ba4fb6f3452
xqz-u <marco.gallo0530@gmail.com>
2022-11-30 19:50:46 +0100

    transfer learn resnet18 imagenet cifar100 first implementation

--- assignment2/part1/cifar100_utils.py
+++ assignment2/part1/cifar100_utils.py
@@ -15,10 +15,9 @@
 ################################################################################
 
 import torch
-
-from torchvision.datasets import CIFAR100
 from torch.utils.data import random_split
 from torchvision import transforms
+from torchvision.datasets import CIFAR100
 
 
 def add_augmentation(augmentation_name, transform_list):
@@ -44,7 +43,9 @@ def add_augmentation(augmentation_name, transform_list):
     #######################
 
 
-def get_train_validation_set(data_dir, validation_size=5000, augmentation_name=None):
+def get_train_validation_set(
+    data_dir, validation_size=5000, augmentation_name=None, **_
+):
     """
     Returns the training and validation set of CIFAR100.
 
@@ -61,32 +62,50 @@ def get_train_validation_set(data_dir, validation_size=5000, augmentation_name=N
     mean = (0.5071, 0.4867, 0.4408)
     std = (0.2675, 0.2565, 0.2761)
 
-    train_transform = [transforms.Resize((224, 224)),
-                       transforms.ToTensor(),
-                       transforms.Normalize(mean, std)]
+    train_transform = [
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std),
+    ]
     if augmentation_name is not None:
         add_augmentation(augmentation_name, train_transform)
     train_transform = transforms.Compose(train_transform)
 
-    val_transform = transforms.Compose([transforms.Resize((224, 224)),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(mean, std)])
+    val_transform = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std),
+        ]
+    )
 
-    # We need to load the dataset twice because we want to use them with different transformations
-    train_dataset = CIFAR100(root=data_dir, train=True, download=True, transform=train_transform)
-    val_dataset = CIFAR100(root=data_dir, train=True, download=True, transform=val_transform)
+    # We need to load the dataset twice because we want to use them with
+    # different transformations
+    train_dataset = CIFAR100(
+        root=data_dir, train=True, download=True, transform=train_transform
+    )
+    val_dataset = CIFAR100(
+        root=data_dir, train=True, download=True, transform=val_transform
+    )
 
     # Subsample the validation set from the train set
     if not 0 <= validation_size <= len(train_dataset):
-        raise ValueError("Validation size should be between 0 and {0}. Received: {1}.".format(
-            len(train_dataset), validation_size))
+        raise ValueError(
+            "Validation size should be between 0 and {0}. Received: {1}.".format(
+                len(train_dataset), validation_size
+            )
+        )
 
-    train_dataset, _ = random_split(train_dataset,
-                                    lengths=[len(train_dataset) - validation_size, validation_size],
-                                    generator=torch.Generator().manual_seed(42))
-    _, val_dataset = random_split(val_dataset,
-                                  lengths=[len(val_dataset) - validation_size, validation_size],
-                                  generator=torch.Generator().manual_seed(42))
+    train_dataset, _ = random_split(
+        train_dataset,
+        lengths=[len(train_dataset) - validation_size, validation_size],
+        generator=torch.Generator().manual_seed(42),
+    )
+    _, val_dataset = random_split(
+        val_dataset,
+        lengths=[len(val_dataset) - validation_size, validation_size],
+        generator=torch.Generator().manual_seed(42),
+    )
 
     return train_dataset, val_dataset
 
@@ -104,9 +123,15 @@ def get_test_set(data_dir):
     mean = (0.5071, 0.4867, 0.4408)
     std = (0.2675, 0.2565, 0.2761)
 
-    test_transform = transforms.Compose([transforms.Resize((224, 224)),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(mean, std)])
+    test_transform = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std),
+        ]
+    )
 
-    test_dataset = CIFAR100(root=data_dir, train=False, download=True, transform=test_transform)
+    test_dataset = CIFAR100(
+        root=data_dir, train=False, download=True, transform=test_transform
+    )
     return test_dataset
--- assignment2/part1/train.py
+++ assignment2/part1/train.py
@@ -15,15 +15,34 @@
 ################################################################################
 
 import argparse
+import logging
+import os
+from pprint import pprint
+from typing import Dict, List, Union
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data as data
-import torchvision.models as models
+import torchvision.models as tvmodels
 
 from cifar100_utils import get_test_set, get_train_validation_set
+
+Metrics = Dict[str, Union[float, np.ndarray, torch.Tensor]]
+Metrics = Dict[str, Union[float, np.ndarray, torch.Tensor]]
+MetricsDict = Union[Metrics, Dict[str, "MetricsDict"]]
+
+
+logger = logging.getLogger("Resnet18-ImageNet->Cifar100")
+
+
+def setup_root_logging(level: int = logging.INFO):
+    logging.basicConfig(
+        level=level,
+        format="p%(process)s [%(asctime)s] [%(levelname)s]  %(message)s  (%(name)s:%(lineno)s)",
+        datefmt="%y-%m-%d %H:%M",
+    )
 
 
 def set_seed(seed):
@@ -39,30 +58,65 @@ def set_seed(seed):
     torch.backends.cudnn.benchmark = False
 
 
+def make_dataloader(
+    dataset: data.Dataset, batch_size: int, shuffle=True
+) -> data.DataLoader:
+    return data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+
 def get_model(num_classes=100):
     """
     Returns a pretrained ResNet18 on ImageNet with the last layer
     replaced by a linear layer with num_classes outputs.
     Args:
-        num_classes: Number of classes for the final layer (for CIFAR100 by default 100)
+        num_classes: Number of classes for the final layer (for CIFAR100 by
+        default 100)
     Returns:
         model: nn.Module object representing the model architecture.
     """
-    #######################
-    # PUT YOUR CODE HERE  #
-    #######################
 
     # Get the pretrained ResNet18 model on ImageNet from torchvision.models
-    pass
+    trained_weights = tvmodels.ResNet18_Weights.DEFAULT
+    model = tvmodels.resnet18(weights=trained_weights)
 
     # Randomly initialize and modify the model's last layer for CIFAR100.
-    pass
-
-    #######################
-    # END OF YOUR CODE    #
-    #######################
+    model.fc.weight = nn.Parameter(torch.normal(0.0, 0.01, (num_classes, 512)))
+    model.fc.bias = nn.Parameter(torch.zeros_like(model.fc.bias))
+    for name, param in model.named_parameters():
+        if name not in {"fc.weight", "fc.bias"}:
+            param.requires_grad = False
 
     return model
+
+
+def train_one_epoch(
+    train_dataloader: data.DataLoader,
+    model: nn.Module,
+    optimizer: torch.optim.Optimizer,
+    running_loss_period: int = 50,
+    **kwargs,
+) -> MetricsDict:
+    loss_module = kwargs["loss_module"]
+    epoch_loss, running_loss, datapoints = 0.0, 0.0, 0
+    for i, (batch, labels) in enumerate(train_dataloader):
+        batch = batch.to(kwargs["device"])
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        # forward + backward + optimize
+        predictions = model(batch)
+        loss = loss_module(predictions, labels)
+        loss.backward()
+        optimizer.step()
+        datapoints += len(batch)
+        epoch_loss += loss.item() * len(batch)
+        running_loss += loss.item()
+        if i % running_loss_period == running_loss_period - 1:
+            mean_running_loss = running_loss / running_loss_period
+            logger.debug(
+                "[%d %d train] loss: %.3f", i + 1, datapoints, mean_running_loss
+            )
+            running_loss = 0.0
+    return {"train_loss": epoch_loss / datapoints}
 
 
 def train_model(
@@ -83,37 +137,53 @@ def train_model(
         lr: Learning rate to use in the optimizer.
         batch_size: Batch size to train the model with.
         epochs: Number of epochs to train the model for.
-        data_dir: Directory where the dataset should be loaded from or downloaded to.
+        data_dir: Directory where the dataset should be loaded from or
+                  downloaded to.
         checkpoint_name: Filename to save the best model on validation.
         device: Device to use.
         augmentation_name: Augmentation to use for training.
     Returns:
         model: Model that has performed best on the validation set.
     """
-    #######################
-    # PUT YOUR CODE HERE  #
-    #######################
 
     # Load the datasets
-    pass
+    train_dataset, val_dataset = get_train_validation_set(
+        data_dir, augmentation_name=augmentation_name
+    )
+    train_loader = make_dataloader(train_dataset, batch_size=batch_size)
+    val_loader = make_dataloader(val_dataset, batch_size=batch_size)
 
     # Initialize the optimizer (Adam) to train the last layer of the model.
-    pass
+    optim = torch.optim.Adam(lr=lr)
+    loss_module = nn.CrossEntropyLoss()
+
+    args = {"loss_module": loss_module, "device": device}
+    checkpoint_fname = os.path.join(data_dir, checkpoint_name)
 
     # Training loop with validation after each epoch. Save the best model.
-    pass
+    best_params: List[np.ndarray] = None
+    train_losses, val_accuracies = [], []
+    for epoch in range(epochs):
+        model.train()
+        logger.debug("Epoch: %s", epoch)
+        train_metrics = train_one_epoch(train_loader, model, optim, **args)
+        train_losses.append(train_metrics["train_loss"])
+        logger.info("[%d train     ] mean loss: %.3f", epoch, train_losses[-1])
+        train_metrics.update(
+            evaluate_model(model, val_loader, mode="validation", **args)
+        )
+        accuracy = train_metrics["accuracy"]
+        if best_params is None or accuracy > val_accuracies[-1]:
+            torch.save(model.state_dict(), checkpoint_fname)
+            logger.debug("[Epoch %s] updated best model: %s", epoch, checkpoint_fname)
+        val_accuracies.append(accuracy)
 
     # Load the best model on val accuracy and return it.
-    pass
-
-    #######################
-    # END OF YOUR CODE    #
-    #######################
-
+    model.load_state_dict(torch.load(checkpoint_fname))
     return model
 
 
-def evaluate_model(model, data_loader, device):
+def evaluate_model(model, data_loader, device, **kwargs):
     """
     Evaluates a trained model on a given dataset.
 
@@ -125,19 +195,37 @@ def evaluate_model(model, data_loader, device):
         accuracy: The accuracy on the dataset.
 
     """
-    #######################
-    # PUT YOUR CODE HERE  #
-    #######################
-    # Set model to evaluation mode (Remember to set it back to training mode in the training loop)
-    pass
+
+    mode, epoch, loss_module = (
+        kwargs["mode"],
+        kwargs.get("epoch", None),
+        kwargs["loss_module"],
+    )
+    loss, datapoints = 0.0, 0
+    accuracies = torch.zeros(len(data_loader))
+    # Set model to evaluation mode (Remember to set it back to training mode in
+    # the training loop)
+    model.eval()
 
     # Loop over the dataset and compute the accuracy. Return the accuracy
     # Remember to use torch.no_grad().
-    pass
+    with torch.no_grad():
+        for i, (batch, labels) in enumerate(data_loader):
+            batch = batch.to(device)
+            logits = model(batch)
+            loss += loss_module(logits, labels).item() * len(batch)
+            datapoints += len(batch)
+            predictions = logits.argmax(0)
+            accuracies[i] = (predictions == labels).sum() / predictions.sum()
 
-    #######################
-    # END OF YOUR CODE    #
-    #######################
+    accuracy = accuracies.mean()
+    logger.info(
+        "[%d %s] mean loss: %.3f accuracy: %.3f",
+        epoch,
+        mode,
+        loss / datapoints,
+        accuracy,
+    )
 
     return accuracy
 
@@ -150,34 +238,36 @@ def main(lr, batch_size, epochs, data_dir, seed, augmentation_name):
         lr: Learning rate to use in the optimizer.
         batch_size: Batch size to train the model with.
         epochs: Number of epochs to train the model for.
-        data_dir: Directory where the CIFAR10 dataset should be loaded from or downloaded to.
+        data_dir: Directory where the CIFAR10 dataset should be loaded from or
+                  downloaded to.
         seed: Seed for reproducibility.
         augmentation_name: Name of the augmentation to use.
     """
-    #######################
-    # PUT YOUR CODE HERE  #
-    #######################
+
     # Set the seed for reproducibility
-    pass
-
+    set_seed(seed)
     # Set the device to use for training
-    pass
-
+    device = torch.device("cpu" if not torch.cuda.is_available() else "cuda")
     # Load the model
-    pass
-
+    model = get_model().to(device)
     # Get the augmentation to use
-    pass
-
+    ...
     # Train the model
-    pass
-
+    best_model = train_model(
+        model,
+        lr,
+        batch_size,
+        epochs,
+        data_dir,
+        "resnet18_cifar100_ckpt",
+        device,
+        augmentation_name=augmentation_name,
+    )
     # Evaluate the model on the test set
-    pass
-
-    #######################
-    # END OF YOUR CODE    #
-    #######################
+    test_dataset = get_test_set(data_dir)
+    test_loader = make_dataloader(test_dataset, batch_size)
+    evaluate_model(best_model, test_loader, device)
+    logger.info("Mean test accuracy over %d datapoints", len(test_loader))
 
 
 if __name__ == "__main__":
@@ -203,4 +293,12 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     kwargs = vars(args)
+    pprint(kwargs)
     main(**kwargs)
+
+
+# kwargs = {}
+# seed = 42
+# data_dir = "./data"
+# augmentation_name = None
+# batch_size = 128
