213c9b71d3831ca524e06923f849b4b78ba55cf2
xqz-u <marco.gallo0530@gmail.com>
2022-12-01 14:31:43 +0100

    fix

--- lisa_scripts/jupyter_Lisa_tunnel.sh
+++ lisa_scripts/jupyter_Lisa_tunnel.sh
@@ -4,4 +4,4 @@ gpu_node=$1
 port=$2
 [ -z $port ] && port=8001
 
-ssh -J lcur0339@lisa.surfsara.nl lcur0339@$gpu_node.lisa.surfsara.nl -L $port:localhost:$port
+TERM=xterm-256color ssh -J lcur0339@lisa.surfsara.nl lcur0339@$gpu_node.lisa.surfsara.nl -L $port:localhost:$port
