ac89c8752afc184a055a2854631a092161767fea
xqz-u <marco.gallo0530@gmail.com>
2022-12-01 21:36:48 +0100

    transfer learning with augmentations ready to run

--- assignment2/part1/train_job.sh
+++ assignment2/part1/train_job.sh
@@ -17,12 +17,18 @@ module load Anaconda3/2021.05
 source activate dl2022
 
 root="/scratch/$USER"
-mkdir -p $root
+#mkdir -p $root
 
 code_dir="/home/$USER/uvadlc_practicals_2022/assignment2/part1"
 
 augmentations=(rand_hflip rand_crop color_jitter all)
 
-for aug in "${augmentations[@]}"; do
-    echo "Resnet18 Imagenet-1K -> CIFAR100, method: $aug"
-    python $code_dir/train.py --data_dir $root --augmentation_name $aug
+if [ -z $1 ]; then
+    echo "Resnet18 Imagenet-1K -> CIFAR100"
+    python $code_dir/train.py --data_dir $root
+else
+    for aug in "${augmentations[@]}"; do
+	echo "Resnet18 Imagenet-1K -> CIFAR100, method: $aug"
+	python $code_dir/train.py --data_dir $root --augmentation_name $aug
+    done
+fi
