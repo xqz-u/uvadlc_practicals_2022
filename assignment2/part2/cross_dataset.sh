14d8a3e29d928c4ca818665c80c689cefb97d8ce
xqz-u <marco.gallo0530@gmail.com>
2022-12-02 19:02:12 +0100

    did not use patch size of 1, run only jobs that failed

--- assignment2/part2/cross_dataset.sh
+++ assignment2/part2/cross_dataset.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=01:00:00
 #SBATCH --mem=16000M
 #SBATCH --output=/home/%u/job_logs/%x_%A_%a_%u.out
-#SBATCH --array=0-5
+#SBATCH --array=0-3
 
 module purge
 module load 2021
@@ -23,9 +23,15 @@ mkdir -p $root
 code_dir="/home/$USER/uvadlc_practicals_2022/assignment2/part2"
 
 datasets=(cifar10 cifar100)
-methods=(padding fixed_patch random_patch)
-models=("$code_dir/save/models/padding_30_cifar10_clip_ViT-B" \
-	    "$code_dir/save/models/padding_30_cifar100_clip_ViT-B" \
+methods=(
+    # padding
+    fixed_patch random_patch)
+p_sizes=(
+    # 30
+	 1 1)
+models=(
+    # "$code_dir/save/models/padding_30_cifar10_clip_ViT-B" \
+	    # "$code_dir/save/models/padding_30_cifar100_clip_ViT-B" \
 	    "$code_dir/save/models/fixed_patch_1_cifar10_clip_ViT-B" \
 	    "$code_dir/save/models/fixed_patch_1_cifar100_clip_ViT-B" \
 	    "$code_dir/save/models/random_patch_1_cifar10_clip_ViT-B" \
@@ -34,6 +40,7 @@ models=("$code_dir/save/models/padding_30_cifar10_clip_ViT-B" \
 i=$SLURM_ARRAY_TASK_ID
 model=${models[i]}
 method=${methods[$(( $i / 2 ))]}
+prompt_size=${p_sizes[$(( $i / 2 ))]}
 # shouldn't be necessary but still
 dataset=${datasets[$(( $i / 3 ))]}
 
@@ -42,4 +49,5 @@ echo "$method $dataset $model"
 python cross_dataset.py --dataset $dataset \
        --method $method \
        --evaluate \
-       --resume "$model/32_sgd_lr_40_decay_0_bsz_128_warmup_1000_trial_1/model_best.pth.tar"
+       --resume "$model/32_sgd_lr_40_decay_0_bsz_128_warmup_1000_trial_1/model_best.pth.tar" \
+       --prompt_size $prompt_size
