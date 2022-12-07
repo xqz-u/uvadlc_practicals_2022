11ce68cbd3533cecd6b9a4f293ad7f37c87724d1
xqz-u <marco.gallo0530@gmail.com>
2022-12-07 17:46:55 +0100

    ready to train

--- assignment3/part1/train_pl.py
+++ assignment3/part1/train_pl.py
@@ -17,21 +17,73 @@
 import argparse
 import os
 from pprint import pprint
+from typing import List
 
-# import numpy as np
 import pytorch_lightning as pl
 import torch
-
-# import torch.nn as nn
 import torch.nn.functional as F
-
-# import torchvision.transforms.functional as F
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torchvision.utils import make_grid, save_image
 
 import utils as u
 from cnn_encoder_decoder import CNNDecoder, CNNEncoder
 from mnist import mnist
+
+
+def cl_parser() -> argparse.ArgumentParser:
+    # Feel free to add more argument parameters
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    # Model hyperparameters
+    parser.add_argument(
+        "--z_dim", default=20, type=int, help="Dimensionality of latent space"
+    )
+    parser.add_argument(
+        "--num_filters",
+        default=32,
+        type=int,
+        help="Number of channels/filters to use in the CNN encoder/decoder.",
+    )
+
+    # Optimizer hyperparameters
+    parser.add_argument("--lr", default=1e-3, type=float, help="Learning rate to use")
+    parser.add_argument("--batch_size", default=128, type=int, help="Minibatch size")
+
+    # Other hyperparameters
+    parser.add_argument(
+        "--data_dir",
+        default="../data/",
+        type=str,
+        help="Directory where to look for the data. For jobs on Lisa, this should be $TMPDIR.",
+    )
+    parser.add_argument("--epochs", default=80, type=int, help="Max number of epochs")
+    parser.add_argument(
+        "--seed", default=42, type=int, help="Seed to use for reproducing results"
+    )
+    parser.add_argument(
+        "--num_workers",
+        default=4,
+        type=int,
+        help="Number of workers to use in the data loaders. To have a truly deterministic run, this has to be 0. "
+        + "For your assignment report, you can use multiple workers (e.g. 4) and do not have to set it to 0.",
+    )
+    parser.add_argument(
+        "--log_dir",
+        default="VAE_logs",
+        type=str,
+        help="Directory where the PyTorch Lightning logs should be created.",
+    )
+    parser.add_argument(
+        "--progress_bar",
+        action="store_true",
+        help=(
+            "Use a progress bar indicator for interactive experimentation. "
+            "Not to be used in conjuction with SLURM jobs"
+        ),
+    )
+    return parser
 
 
 class VAE(pl.LightningModule):
@@ -92,7 +144,7 @@ class VAE(pl.LightningModule):
         return L_rec, L_reg, bpd
 
     @torch.no_grad()
-    def sample(self, batch_size):
+    def sample(self, batch_size) -> torch.Tensor:
         """
         Function for sampling a new batch of random images.
         Inputs:
@@ -161,7 +213,7 @@ class GenerateCallback(pl.Callback):
         if (trainer.current_epoch + 1) % self.every_n_epochs == 0:
             self.sample_and_save(trainer, pl_module, trainer.current_epoch + 1)
 
-    def sample_and_save(self, trainer, pl_module, epoch):
+    def sample_and_save(self, trainer, pl_module, epoch) -> torch.Tensor:
         """
         Function that generates and save samples from the VAE.
         The generated sample images should be added to TensorBoard and,
@@ -183,20 +235,11 @@ class GenerateCallback(pl.Callback):
             save_image(
                 grid, os.path.join(trainer.logger.log_dir, f"epoch_{epoch}_samples.png")
             )
+        return grid
 
 
-def train_vae(args):
-    """
-    Function for training and testing a VAE model.
-    Inputs:
-        args - Namespace object from the argument parser
-    """
-
+def make_trainer(args) -> pl.Trainer:
     os.makedirs(args.log_dir, exist_ok=True)
-    train_loader, val_loader, test_loader = mnist(
-        batch_size=args.batch_size, num_workers=args.num_workers, root=args.data_dir
-    )
-
     # Create a PyTorch Lightning trainer with the generation callback
     gen_callback = GenerateCallback(save_to_disk=True)
     save_callback = ModelCheckpoint(
@@ -217,12 +260,31 @@ def train_vae(args):
             + f"progress, check the TensorBoard file at {trainer.logger.log_dir}. If you "
             + 'want to see the progress bar, use the argparse option "progress_bar".\n'
         )
+    return trainer
+
+
+def get_data(args: argparse.Namespace) -> List[torch.utils.data.DataLoader]:
+    return mnist(
+        batch_size=args.batch_size, num_workers=args.num_workers, root=args.data_dir
+    )
+
+
+def train_vae(args: argparse.Namespace):
+    """
+    Function for training and testing a VAE model.
+    Inputs:
+        args - Namespace object from the argument parser
+    """
+    trainer = make_trainer(args)
+    train_loader, val_loader, test_loader = get_data(args)
 
     # Create model
     pl.seed_everything(args.seed)  # To be reproducible
     model = VAE(num_filters=args.num_filters, z_dim=args.z_dim, lr=args.lr)
 
     # Training
+    gen_callback = trainer.callbacks[0]
+    assert isinstance(gen_callback, GenerateCallback)
     gen_callback.sample_and_save(trainer, model, epoch=0)  # Initial sample
     trainer.fit(model, train_loader, val_loader)
 
@@ -243,59 +305,7 @@ def train_vae(args):
 
 
 if __name__ == "__main__":
-    # Feel free to add more argument parameters
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-
-    # Model hyperparameters
-    parser.add_argument(
-        "--z_dim", default=20, type=int, help="Dimensionality of latent space"
-    )
-    parser.add_argument(
-        "--num_filters",
-        default=32,
-        type=int,
-        help="Number of channels/filters to use in the CNN encoder/decoder.",
-    )
-
-    # Optimizer hyperparameters
-    parser.add_argument("--lr", default=1e-3, type=float, help="Learning rate to use")
-    parser.add_argument("--batch_size", default=128, type=int, help="Minibatch size")
-
-    # Other hyperparameters
-    parser.add_argument(
-        "--data_dir",
-        default="../data/",
-        type=str,
-        help="Directory where to look for the data. For jobs on Lisa, this should be $TMPDIR.",
-    )
-    parser.add_argument("--epochs", default=80, type=int, help="Max number of epochs")
-    parser.add_argument(
-        "--seed", default=42, type=int, help="Seed to use for reproducing results"
-    )
-    parser.add_argument(
-        "--num_workers",
-        default=4,
-        type=int,
-        help="Number of workers to use in the data loaders. To have a truly deterministic run, this has to be 0. "
-        + "For your assignment report, you can use multiple workers (e.g. 4) and do not have to set it to 0.",
-    )
-    parser.add_argument(
-        "--log_dir",
-        default="VAE_logs",
-        type=str,
-        help="Directory where the PyTorch Lightning logs should be created.",
-    )
-    parser.add_argument(
-        "--progress_bar",
-        action="store_true",
-        help=(
-            "Use a progress bar indicator for interactive experimentation. "
-            "Not to be used in conjuction with SLURM jobs"
-        ),
-    )
-
+    parser = cl_parser()
     args = parser.parse_args()
     pprint(vars(args))
 
@@ -316,26 +326,3 @@ if __name__ == "__main__":
 #     "z_dim": 20,
 # }
 # args = argparse.Namespace(**kwargs)
-
-
-# batch, labs = next(iter(train_loader))
-# batch_show = batch.squeeze().to(torch.uint8)
-
-
-# model = VAE(num_filters=args.num_filters, z_dim=args.z_dim, lr=args.lr)
-
-# grid = make_grid(batch_show, nrow=4)
-# # u.show(list(grid), cmap="gray")
-
-
-# samples = model.sample(4)
-# samples = samples.float() / 15
-# grid = make_grid(samples, nrow=4, normalize=True, value_range=(0, 1), pad_value=0.5)
-# grid = grid.detach().cpu()
-# u.show(list(grid), cmap="gray")
-
-# pl.seed_everything(args.seed)
-# s = torch.randn((4, 20))
-# pl.seed_everything(args.seed)
-# ss = u.sample_reparameterize(torch.zeros((4, 20)), torch.ones(4, 20))
-# assert (s == ss).all()
