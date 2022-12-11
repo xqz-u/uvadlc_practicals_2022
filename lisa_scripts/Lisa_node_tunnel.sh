713873e6c08b9c17beab7939ea191be3772b6228
xqz-u <marco.gallo0530@gmail.com>
2022-12-11 14:32:22 +0100

    ready to run job on lisa

--- lisa_scripts/Lisa_node_tunnel.sh
+++ lisa_scripts/Lisa_node_tunnel.sh
@@ -4,4 +4,6 @@ gpu_node=$1
 port=$2
 [ -z $port ] && port=8001
 
+echo "Attempt SSH tunnel to GPU node $gpu_node port $port"
+
 TERM=xterm-256color ssh -J lcur0339@lisa.surfsara.nl lcur0339@$gpu_node.lisa.surfsara.nl -L $port:localhost:$port
